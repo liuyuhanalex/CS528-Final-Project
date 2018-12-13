32b2188b0afbfc956fad5d80f5c3bf0cafeb5be3
liuyuhan <yliu26@wpi.edu>
2018-12-12 22:23:23 -0500

    Finish Speech to Text

--- FinalProject/app/src/main/java/com/example/liuyu/finalproject/SpeechToTextActivity.java
+++ FinalProject/app/src/main/java/com/example/liuyu/finalproject/SpeechToTextActivity.java
@@ -1,22 +1,55 @@
 package com.example.liuyu.finalproject;
 
 import android.Manifest;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.speech.RecognizerIntent;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class SpeechToTextActivity extends AppCompatActivity {
 
     final static int AUDIO_REQUEST_CODE = 888;
+    final static int REQ_CODE_SPEECH_INPUT = 111;
+    private TextView voiceInput;
+    private Button speakButton,save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speech_to_text);
+
+        voiceInput = findViewById(R.id.voiceInput);
+        speakButton = findViewById(R.id.btnSpeak);
+        save = findViewById(R.id.speech_text_save);
+
+        speakButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                askSpeechInput();
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("TextFromSpeech",voiceInput.getText().toString());
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+        });
     }
 
     //Using this function to check audio permission
@@ -47,4 +80,34 @@ public class SpeechToTextActivity extends AppCompatActivity {
             }
         }
     }
+
+    private void askSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "You can say something now!");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    voiceInput.setText(result.get(0));
+                }
+                break;
+            }
+        }
+    }
+
+
 }
